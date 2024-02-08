7e5cae4d6f1245686529ccb943c5f540d9de9ba6
Agus Prayogi <agus21apy@gmail.com>
2024-02-08 16:33:57 +0700

    [ADD] initial project

---
+++ src/main/java/io/github/agusprayogi02/learningclass/LearningClassApplication.java
@@ -0,0 +1,13 @@
+package io.github.agusprayogi02.learningclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningClassApplication.class, args);
+	}
+
+}
